9c214bc56179c6df61c2d9669fcfcfad1f07d89a
Temp-975 <36461985+Temp-975@users.noreply.github.com>
2018-02-15 12:05:13 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function startup() {
-    connectButton = document.getElementByID('connectButton);
+    connectButton = document.getElementByID('connectButton');
     disconnectButton = document.getElementById('disconnectButton');
     sendButton = document.getElementById('sendButton');
     messageInputBox = document.getElementById('message');
@@ -8,4 +8,18 @@ function startup() {
     connectButton.addEventListener('click', connectPeers, false);
     disconnectButton.addEventListener('click', disconnectPeers, false);
     sendButton.addEventListener('click', sendMessage, false);
+}
+
+function connectPeer() {
+    localConnection = new RTCPeerConnection();
+    
+    sendChannel = localConnection.createDataChannel('sendChannel');
+    sendChannel.onopen = handleSendChannelStatusChange;
+    sendChannel.onclose = handleSendChannelStatusChange;
+    
+    remoteConnection = new RTCPeerConnection();
+    remoteConnection.ondatachannel = receiveChannelCallback;
+    
+    localConnection.onicecandidate = e => !e.candidate || remoteConnection.addIceCandidate(e.candidate(e.candidate)
+    
 }
